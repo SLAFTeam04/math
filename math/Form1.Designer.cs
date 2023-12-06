cda66318783d3499d4c56b6182964af03c16afbf
software team team4 <softwareteam4.info@gmail.com>
2023-12-06 11:29:21 +0530

    Add project files.

---
+++ math/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace math
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.textBoxUserAnswer = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblQCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber.Location = new System.Drawing.Point(84, 42);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(86, 31);
+            this.lblNumber.TabIndex = 0;
+            this.lblNumber.Text = "label1";
+            // 
+            // textBoxUserAnswer
+            // 
+            this.textBoxUserAnswer.Location = new System.Drawing.Point(70, 97);
+            this.textBoxUserAnswer.Name = "textBoxUserAnswer";
+            this.textBoxUserAnswer.Size = new System.Drawing.Size(100, 20);
+            this.textBoxUserAnswer.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(194, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 81);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(381, 97);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(133, 31);
+            this.lblAnswer.TabIndex = 0;
+            this.lblAnswer.Text = "ANSWER";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(275, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 81);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblQCount
+            // 
+            this.lblQCount.AutoSize = true;
+            this.lblQCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQCount.Location = new System.Drawing.Point(607, 19);
+            this.lblQCount.Name = "lblQCount";
+            this.lblQCount.Size = new System.Drawing.Size(127, 31);
+            this.lblQCount.TabIndex = 0;
+            this.lblQCount.Text = "QcOUNT";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxUserAnswer);
+            this.Controls.Add(this.lblQCount);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblNumber);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.TextBox textBoxUserAnswer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblQCount;
+    }
+}
+
---
+++ math/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace math
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private readonly Random random = new Random();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ShowNumbers();
+        }
+        private Timer timer;
+        private void ShowNumbers()
+        {
+            lblAnswer.Text = string.Empty;
+            lblNumber.Text = string.Empty;  
+            textBoxUserAnswer.Text = string.Empty;
+            timer = new Timer();
+            timer.Interval = 500;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+      
+        private int iterations = 8;
+
+        int CountCorrectAnswer=0;
+        int CountIncorrectAnswer=0;
+        int QestionCount = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QestionCount++;
+            lblQCount.Text = QestionCount.ToString();
+
+            if (QestionCount == 3)
+            {
+                MessageBox.Show("Correct Answers: " + CountCorrectAnswer.ToString() + "\n" + "Incorrect Answers: " + CountIncorrectAnswer.ToString());
+            }
+            else
+            {
+                int inputAnswer = Convert.ToInt32(textBoxUserAnswer.Text);
+                if (Answer == inputAnswer)
+                {
+                    CountCorrectAnswer++;
+                }
+                else
+                {
+                    CountIncorrectAnswer++;
+                }
+
+                iterations = 8;
+
+               
+                if (QestionCount < 3)
+                {
+                    ShowNumbers();
+                }
+            }
+
+
+        }
+        int Answer = 0;
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            int randomNumber = random.Next(-10, 11);
+            lblNumber.Text = randomNumber.ToString();
+            Answer += randomNumber;           
+            iterations--;           
+            if (iterations <= 0)
+            {
+                ((Timer)sender).Stop(); 
+                lblAnswer.Text = Answer.ToString();                
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            iterations = 8;
+            ShowNumbers();
+        }
+    }
+}
